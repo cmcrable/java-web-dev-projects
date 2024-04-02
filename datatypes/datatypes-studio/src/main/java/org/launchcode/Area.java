557244d9b8ce99ce55a8fa063366c82cc5e0a804
Curtiss <cmcrable@gmail.com>
2024-04-01 20:32:29 -0500

    Crable Unit 2 Ch2 Studio - Area of a Circle

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -6,10 +6,16 @@ public class Area {
 //        double pi = 3.14159;
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter a Radius value: ");
-        double radius = input.nextInt();
+        double radius = input.nextDouble();
 //        double area = pi * radius * radius;
         double area = Circle.getArea(radius);
 
-        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+            while (radius < 0) {
+                System.out.println("Cannot process a negative number. Please try again.");
+                System.out.println("Please enter a Radius value: ");
+                radius = input.nextDouble();
+            }
+            System.out.println("The area of a circle of radius " + radius + " is: " + area);
+
     }
 }
